03a083e27057bc35431430016d4bdbcb82f5c9c6
lucashulej <lucashulej>
2019-08-22 17:49:54 -0300

    faltan hasta el 15

---
+++ Hulej.Lucas/Ejercicio_14/CalculoDeArea.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_14
+{
+    class CalculoDeArea
+    {
+        public static double CalcularCuadrado(double numero)
+        {
+            double retorno = 0;
+            return retorno;
+        }
+    }
+}
