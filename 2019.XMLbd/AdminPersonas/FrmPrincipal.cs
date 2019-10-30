2d4e870df9c63e6f476b5cd1a578a5513b83112b
alumno <alumno@utnfra.com.ar>
2019-10-30 12:09:46 -0300

    .

---
+++ 2019.XMLbd/AdminPersonas/FrmPrincipal.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Entidades;
+
+namespace AdminPersonas
+{
+    public partial class FrmPrincipal : Form
+    {
+        private List<Persona> lista;
+
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+
+            this.IsMdiContainer = true;
+            this.WindowState = FormWindowState.Maximized;
+
+            this.lista = new List<Persona>();
+        }
+
+        private void cargarArchivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //implementar...
+        }
+
+        private void guardarEnArchivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //implementar...
+        }
+
+        private void visualizarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmVisorPersona frm = new frmVisorPersona();
+
+            frm.StartPosition = FormStartPosition.CenterScreen;
+
+            //implementar
+
+            frm.Show();
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //implementar
+        }
+    }
+}
